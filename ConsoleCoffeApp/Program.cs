2ed337b5895a0b4d2392bc5217f00852348e4ffa
Edoardo Zanotti <62666529+edozanox@users.noreply.github.com>
2020-04-08 18:22:22 +0200

    User stories 10 (menù navigabile) 11 (riepilogo info) Modifica elementi da enum ad array

--- ConsoleCoffeApp/Program.cs
+++ ConsoleCoffeApp/Program.cs
@@ -8,79 +8,142 @@ namespace ConsoleCoffeApp
 {
     class Program
     {
+        
         public const string Accesso = "BENVENUTO!";
         public const string Errore = "ERROR: Voce selezionata inesistente!";
+        public const string AnotherTimeReq = "Vuoi effettuare un'altra scelta? (Y/N)";
         public const string GuidaMenuUno = "Digita la voce desiderata: ";
         public const string GuidaMenuProdotto = "Digita il prodotto richiesto: ";
+        
         public enum TipoMenu { SNACK, DRINK, CAFFETTERIA };
-        public enum Drink { VINO_ROSSO, VINO_BIANCO, APEROL, CAMPARI, BIRRA}
-        public enum Snack { TOAST, PIADINA, PANINO }
-        public enum Caffetteria { CAFFE, THE_BIANCO, THE_VERDE, CIOCCOLATA }
+    
 
 
         static void Main(string[] args)
         {
-            
+            string[] drink = { "VINO ROSSO", "VINO BIANCO", "APEROL", "CAMPARI", "BIRRA" };
+            string[] snack = { "TOAST", "PIADINA", "PANINO" };
+            string[] caffetteria = { "CAFFE", "THE BIANCO", "THE VERDE", "CIOCCOLATA" };
+            List<string> ProdottiSelezionati = new List<string>();
+            bool AnotherTime = false;
+            bool IsOK = false;
+            string selezione;            
+            int NumProdSel = 0;
+
             Console.WriteLine(Accesso);
             Console.WriteLine("\n");
 
-            foreach (var tipo in Enum.GetValues(typeof(TipoMenu)))
+            do
             {
-                Console.WriteLine(tipo.ToString());
-                
+                foreach (var tipo in Enum.GetValues(typeof(TipoMenu)))
+                {
+                    Console.WriteLine(tipo.ToString());
+
+                }
+
+                Console.WriteLine("\n");
+                Console.Write(GuidaMenuUno);
+                selezione = Console.ReadLine();
+
+                switch (selezione)
+                {
+                    case "DRINK":
+
+                        for(int i = 0; i < drink.Length; i++)                        
+                            Console.WriteLine(drink[i]);
+                        
+                        do
+                        {
+                            Console.WriteLine("\n");
+                            Console.Write(GuidaMenuProdotto);
+                            selezione = Console.ReadLine();
+                            for (int j = 0; j < drink.Length; j++)
+                            {
+                                if (selezione == drink[j])
+                                {
+                                    ProdottiSelezionati.Add(drink[j]);
+                                    IsOK = true;
+                                }
+                            }
+                        } while (!IsOK);
+
+                        break;
+
+                    case "SNACK":
+                        for(int k = 0; k< snack.Length; k++)                        
+                            Console.WriteLine(snack[k]);                            
+                        
+                        do
+                        {
+                            Console.WriteLine("\n");
+                            Console.Write(GuidaMenuProdotto);
+                            selezione = Console.ReadLine();
+
+                            for (int j = 0; j < snack.Length; j++)
+                            {
+                                if (selezione == snack[j])
+                                {
+                                    ProdottiSelezionati.Add(snack[j]);
+                                    IsOK = true;
+                                }
+                            }                            
+                        } while (!IsOK);
+
+                        break;
+
+                    case "CAFFETTERIA":
+                        for(int j = 0; j < caffetteria.Length; j++)                        
+                            Console.WriteLine(caffetteria[j]); 
+                        do
+                        {
+
+                            Console.WriteLine("\n");
+                            Console.Write(GuidaMenuProdotto);
+                            selezione = Console.ReadLine();
+
+                            for (int j = 0; j < caffetteria.Length; j++)
+                            {
+                                if (selezione == caffetteria[j])
+                                {
+                                    ProdottiSelezionati.Add(caffetteria[j]);
+                                    IsOK = true;
+                                }
+                            }
+                        } while (!IsOK);
+                     
+                        break;
+
+                    default:
+                        Console.WriteLine(Errore);
+                        Console.ReadLine();
+                        break;
+                }
+                Console.WriteLine("\n");
+                Console.Write(AnotherTimeReq);
+                selezione = Console.ReadLine();
+
+                if (selezione == "y" || selezione == "Y")
+                {
+                    AnotherTime = true;
+                    Console.WriteLine($"------------------------------------");
+                }
+                else
+                    AnotherTime = false;               
+
+            } while (AnotherTime);
+
+            NumProdSel = ProdottiSelezionati.Count;
+            Console.WriteLine($"------------------------------------");
+            Console.WriteLine("RIEPILOGO PRODOTTI:");
+            foreach (var elem in ProdottiSelezionati)
+            {
+                Console.WriteLine($">>[{elem}]");
             }
 
             Console.WriteLine("\n");
-            Console.Write(GuidaMenuUno);
-            string selezione = Console.ReadLine();
-
-            switch (selezione)
-            {
-                case "drink":
-                    foreach (var prodotto in Enum.GetValues(typeof(Drink)))
-                    {
-                        Console.WriteLine(prodotto.ToString());
-                        
-
-                    }                    
-                    Console.WriteLine(GuidaMenuProdotto);
-                    Console.ReadLine();
-
-                    break;
-
-                case "snack":
-                    foreach (var prodotto in Enum.GetValues(typeof(Snack)))
-                    {
-                        Console.WriteLine(prodotto.ToString());
-                        Console.ReadLine();
-                        
-                    }
-                    Console.WriteLine(GuidaMenuProdotto);
-                    Console.ReadLine();
-                    break;
-
-                case "caffetteria":
-                    foreach (var prodotto in Enum.GetValues(typeof(Caffetteria)))
-                    {
-                        Console.WriteLine(prodotto.ToString());
-                        Console.ReadLine();
-
-                    }
-                    Console.WriteLine("\n");
-                    Console.WriteLine(GuidaMenuProdotto);
-                    Console.ReadLine();
-                    break;
-
-                default:
-                    Console.WriteLine(Errore);
-                    Console.ReadLine();
-                    break;
-            }
-
-
-
-
-
+            Console.WriteLine($"TOT:  00{NumProdSel} PRODOTTI.");            
+            Console.WriteLine("ARRIVEDERCI");
+            Console.ReadLine();
         }
     }
 }
